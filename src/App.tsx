62524f57f67f381871c1ad2c5b630f5482be1fd4
Tobbie91 <oluwatobiakinrimisi@gmail.com>
2025-07-17 09:21:24 +0100

    meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,25 @@ import ProductsSection from "./components/ProductsSection";
 import AboutSection from "./components/AboutSection";
 import ContactSection from "./components/ContactSection";
 import Navbar from "./components/Navbar";
+import { Helmet } from "react-helmet";
 
 export default function App() {
   return (
-    <div className="font-sans scroll-smooth transition-all duration-700 bg-gradient-to-b from-orange-50 to-white text-brown">
-      <Navbar />
-      <div className="pt-20">
-        <HeroSection />
-        <ProductsSection />
-        <AboutSection />
-        <ContactSection />
+    <>
+      <Helmet>
+        <title>Fave Bhite Kulikuli</title>
+        <meta name="description" content="Healthy Nigerian kulikuli, perfect for stores, snacks, and distribution." />
+      </Helmet>
+
+      <div className="font-sans scroll-smooth transition-all duration-700 bg-gradient-to-b from-orange-50 to-white text-brown">
+        <Navbar />
+        <div className="pt-20">
+          <HeroSection />
+          <ProductsSection />
+          <AboutSection />
+          <ContactSection />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
